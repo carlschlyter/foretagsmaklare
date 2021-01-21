1dd4697aa3e88a1bd385b7edded5d365d8496cd5
Carl Schlyter <carl.olof.schlyter@hotmail.com>
2021-01-21 11:50:11 +0100

    Added slide in - slide out sidebar for mobile view.

---
+++ js/main.js
@@ -0,0 +1,8 @@
+
+    function openSlideMenu(){
+        document.querySelector('#side-menu').style.width = '250px';
+    }
+
+    function closeSlideMenu(){
+        document.querySelector('#side-menu').style.width = '0';
+    }
